6e2ec9c1fc75d9d20c5055c20bba24e1761234f7
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2025-07-05 23:38:44 +0500

    Home page created

---
+++ assets/js/slider.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
+	speed: 2000,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+});
