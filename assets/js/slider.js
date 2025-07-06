37e513cb7a228f5a397172009bdd99f3debda900
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2025-07-06 15:38:51 +0500

    Home page completed

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -15,3 +15,18 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
       }
 });
+
+var swiper = new Swiper(".spotlights-swiper", {
+	slidesPerView: 3,
+    spaceBetween: 15,
+	speed: 2000,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+	navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+});
