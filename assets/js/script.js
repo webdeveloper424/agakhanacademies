19e1b8644749d41b54580c746faa3cb5a42e8f3c
atif-hanif <atifhanif74@gmail.com>
2025-09-26 11:31:57 +0500

    Quick facts and Work with us pages updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,42 @@ $(function() {
 if ('scrollRestoration' in history) {
     history.scrollRestoration = 'auto'; // let the browser do it
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration, decimals = 0) {
+        let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        // make increment a decimal-friendly step:
+        increment = range / (duration / 16), // 60fps-ish
+        startTime = null;
+
+        function animateCounter(timestamp) {
+            if (!startTime) startTime = timestamp;
+            let progress = timestamp - startTime;
+            current = start + increment * (progress / 16);
+
+            // clamp when over
+            if ((increment > 0 && current >= end) || (increment < 0 && current <= end)) {
+                current = end;
+            }
+
+            obj.textContent = current.toFixed(decimals);
+
+            if (current !== end) {
+                requestAnimationFrame(animateCounter);
+            }
+        }
+
+        requestAnimationFrame(animateCounter);
+  }
+    counter("count1", 0, 7, 1000);
+    counter("count2", 0, 1, 1000);
+    counter("count3", 0, 1895, 1000);
+    counter("count4", 0, 99, 1000);
+    counter("count5", 0, 4, 1000);
+    counter("count6", 0, 693, 1000);
+    counter("count7", 0, 32.9, 1000, 1);
+    counter("count8", 0, 97, 1000);
+    counter("count9", 0, 13, 1000);
+});
